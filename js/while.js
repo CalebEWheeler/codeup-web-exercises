93962934117184f26971a8a3f809cffbeaf58a0b
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-08-19 11:48:25 -0500

    completed loops exercises after Trant's walkthrough.

--- js/while.js
+++ js/while.js
@@ -1,7 +1,29 @@
-function whileLoop(input) {
-    let output = '';
-    let i = 2;
-    console.log(2 * i);
-    i++;
+// function check() {
+//     var i = 1
+//     while (i < 65536) {
+//         console.log(i * 2 + '\n');
+//         i += i++;
+//     }
+// }
+function powersOfTwo() {
+    let num = 2
+    while (num <= 65536) {
+        console.log(num)
+        num *= 2
+    }
 }
-whileLoop();
+
+function sellCones() {
+    var allCones = Math.floor(Math.random() * 50) + 50;
+    do {
+        var numConesWanted = Math.floor(Math.random() * 5) + 1;
+        if (numConesWanted <= allCones) {
+            console.log(numConesWanted + 'cones sold...')
+            allCones -= numConesWanted
+        } else {
+            console.log('Cannot sell you ' + numConesWanted + ' cones I only have ' + allCones + '...')
+        }
+
+    } while(allCones > 0);
+    console.log("Yay! I sold them all!")
+}
---
+++ tests/while_tests.js
@@ -0,0 +1,26 @@
+// describe('check', function() {
+//     it('check should be a defined function', function() {
+//         expect(typeof check).toBe('function')
+//     })
+//     it('check should have a while loop', function() {
+//         expect(check()).toContain('while')
+//     })
+// })
+
+describe('powersOfTwo', function() {
+    it('powersOfTwo should be a function', function() {
+        expect(typeof powersOfTwo).toBe('function')
+    })
+    it('powersOfTwo() should return undefined', function() {
+        expect(powersOfTwo()).toBe(undefined)
+    })
+})
+
+describe('sellCones', function() {
+    it('sellCones should be a function', function() {
+        expect(typeof sellCones).toBe('function')
+    })
+    it('sellCones() should return undefined', function() {
+        expect(sellCones()).toBe(undefined)
+    })
+})
