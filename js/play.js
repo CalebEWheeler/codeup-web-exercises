7834b2bddacb6cc5f3671dd0625544d7e6a352a7
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-08-25 10:21:11 -0500

    added defuse-the-bom.html to intelliJ

--- js/play.js
+++ js/play.js
@@ -51,21 +51,49 @@
 // battleRoyal();
 
 //js pushup 8/25
-function fizzBuzz(num) {
-    for (var i=1; i<=100; i++) {
-    if(i %3 === 0 && i %5 === 0) {
-            console.log("fizz" +"buzz")
-        }
-        else if (i %3 === 0) {
-            var fizz = console.log("fizz")
-        }
-        else if( i %5 === 0) {
-            var buzz = console.log("buzz")
-        }
-        else if (i %3 !== 0 && i %5 !== 0) {
-        console.log(i);
-    }
+// function fizzBuzz(num) {
+//     for (var i=1; i<=100; i++) {
+//     if(i %3 === 0 && i %5 === 0) {
+//             console.log("fizz" +"buzz")
+//         }
+//         else if (i %3 === 0) {
+//             var fizz = console.log("fizz")
+//         }
+//         else if( i %5 === 0) {
+//             var buzz = console.log("buzz")
+//         }
+//         else if (i %3 !== 0 && i %5 !== 0) {
+//         console.log(i);
+//     }
+//
+//     }
+// }
+// console.log(fizzBuzz());
 
-    }
-}
-console.log(fizzBuzz());
+// 'use strict';
+// // this code will produce a console log every second
+// // when count >= max, the interval is cancelled, and the logging will stop
+// var count = 0;
+// var max = 5;
+// var interval = 3000; // interval time in milliseconds
+// var intervalId = setInterval(myFunc, interval);
+// function myFunc() {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//         console.log('All done');
+//     } else {
+//         count++;
+//         alert('Repeating this line ' + count);
+//     }
+// }
+
+//write code that will delay an alert for three seconds
+// var delay = 5000;
+// var timeOutID = setTimeout(function () {
+//     // alert("Hello, we've hit three seconds");
+//
+//     // redirect browser to google.com
+//     window.location = 'http://www.google.com';
+// }, delay);
+
+
