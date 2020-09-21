17a2300a9f3ec444ac24fcb6a5767225286d81fb
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-09-21 13:59:42 -0500

    create a function named wait that sets a num as a parameter, and return a promise that resolves after the passed number of milliseconds

--- js/promises.js
+++ js/promises.js
@@ -11,3 +11,16 @@ fetch('https://api.github.com/users/calebewheeler/events', {headers: GIT_TOKEN})
 }
 getLastGitCommit();
 
+const wait = function (num) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (num)
+                resolve();
+            else
+                reject();
+
+        }, num)
+    })
+}
+wait(1000).then(() => console.log("You'll see this after 1 second"))
+wait(3000).then(() => console.log("You'll see this after 3 seconds"))
